42704006959268efadd4cd09b22976fff3e122b7
ZZR-china <2467254599@qq.com>
2016-09-22 17:22:10 +0800

    juejin init

---
+++ README.md
@@ -0,0 +1,56 @@
+
+> A Blog CMS Powered By Vue.js
+
+> 一个前端基于Vue.js，后端基于Node.js的博客内容管理器
+
+### [Demo](http://115.28.90.175:90/#!/)
+### 登陆地址在Demo页面最下方
+
+## Features
+
+* 支持MarkDown语法编辑
+* 博客页面对移动端优化
+* 支持代码高亮
+* 就是要酷炫~~
+
+###### 前端：Vue全家桶
+* Vue.js
+* Vue-Cli
+* Vue-Resource
+* Vue-Validator
+* Vue-Router
+* Vuex
+* Vue-loader
+
+###### 后端
+* Node.js
+* mongoDB (mongoose)
+* Express
+
+###### 工具和语言
+* Webpack
+* ES6
+* SASS
+* Jade
+
+
+进入项目目录
+```
+cd CMS-of-Blog
+```
+安装依赖
+```
+npm install
+```
+进入服务器所在文件夹并运行服务器
+```
+cd server
+node www
+```
+打开浏览器输入http://localhost:3000/
+如果一切OK，则进入博客的归档页面，登陆按钮在最下方。
+###### 注意
+* **第一次从node启动时，数据库会初始化，自动插入两个用户，分别是和'admin'，'游客'，前者有管理员权限，默认密码是111.**
+* 如果需要改动，则在第一次启动前打开server目录下的init.js，初始化数据放在该文件中。
+* 推荐[MongoChef](http://3t.io/mongochef/)作为mongoDB可视化管理工具
+* 如果有疑问，欢迎交流~
---
+++ server/db.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    init = require('./init'),
+
+    userSchema = new Schema({
+        name: String,
+        password: String,
+    }, {
+        collection: 'users'
+    }),
+
+    articleSchema = new Schema({
+        title: String,
+        date: Date,
+        content: String,
+    }),
+
+    linkSchema = new Schema({
+        name: String,
+        href: String,
+    }),
+
+    User = mongoose.model('User', userSchema),
+    Article = mongoose.model('Article', articleSchema),
+    Link = mongoose.model('Link', linkSchema),
+
+    admin = new User(init.admin),
+    visitor = new User(init.visitor),
+    article = new Article(init.article),
+    link = new Link(init.link);
+
+mongoose.connect('mongodb://blog:950127@ds139425.mlab.com:39425/blog')
+mongoose.set('debug', true)
+
+
+var db = mongoose.connection;
+db.on('error', function() {
+    console.log('error')
+})
+db.once('open', function() {
+    console.log('opened')
+        //存储用户信息
+
+    User.find(null, function(err, doc) {
+        if (err) {
+            console.log(err)
+        } else {
+            if (!doc[0]) {
+                visitor.save(function(err) {
+                    if (err) return console.log(err)
+                })
+
+                link.save(function(err) {
+                    if (err) return console.log(err)
+                })
+
+                admin.save(function(err) {
+                    if (err) return console.log(err)
+                })
+                article.save(function(err) {
+                    if (err) return console.log(err)
+                })
+            }
+        }
+    })
+})
+
+module.exports = {
+    User: User,
+    Article: Article,
+    Link: Link
+}
